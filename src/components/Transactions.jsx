201d4bb792cab47c2cec96eb861820a935a434b3
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-22 00:28:05 +0300

    added transactions table and added styling

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,36 @@
+function Transactions() {
+
+
+    return (
+        <div>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>date</th>
+                        <th>Description</th>
+                        <th>category</th>
+                        <th>amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2019-12-01</td>
+                        <td>Paycheck from Bob's Burgers</td>
+                        <td>Income</td>
+                        <td>1000</td>
+                    </tr>
+                    <tr>
+                        <td>2019-12-01</td>
+                        <td>South by Southwest Quinoa Bowl at Fresh & Co</td>
+                        <td>Food</td>
+                        <td>10.55</td>
+                    </tr>
+                </tbody>
+                
+            </table>
+            
+        </div>
+    )
+}
+
+export default Transactions;
