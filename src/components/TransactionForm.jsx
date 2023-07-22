e8d3ff3226b2820ad1dcb0981cca07881f2130c8
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-22 15:39:29 +0300

    clear form inputs after submititng

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -8,18 +8,29 @@ function TransactionsForm({ transactionsData, setTransactionsData }) {
         amount: 0,
     })
 
+    const { date, description, category, amount } = formDetails;
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         // console.log(formDetails)
 
+        // copy formDetails and add new key-value id
         const updatedFormDetails = {...formDetails, id: transactionsData.length + 1}
 
         const updatedTransactionsData = [...transactionsData, updatedFormDetails]
 
-        console.log(updatedTransactionsData)
+        // console.log(updatedTransactionsData)
 
         setTransactionsData(updatedTransactionsData)
+
+        // clear form inputs after submitting
+        setFormDetails({ 
+            date: "",
+            description: "",
+            category: "",
+            amount: 0
+        })
     }
 
     const changeFormDetails = (event) => {
@@ -31,22 +42,22 @@ function TransactionsForm({ transactionsData, setTransactionsData }) {
             <h2>Add Transaction</h2>
             <div>
                 <label htmlFor="date">Date</label>
-                <input id="date" name="date" type="date" onChange={changeFormDetails}/>
+                <input id="date" name="date" type="date" onChange={changeFormDetails} value={date} />
             </div>
 
             <div>
                 <label htmlFor="description">Description</label>
-                <textarea id="description" name="description" onChange={changeFormDetails} />
+                <textarea id="description" name="description" onChange={changeFormDetails} value={description}/>
             </div>
 
             <div>
                 <label htmlFor="category">Category</label>
-                <input id="category" type="text" name="category" onChange={changeFormDetails} />
+                <input id="category" type="text" name="category" onChange={changeFormDetails} value={category}/>
             </div>
 
             <div>
                 <label htmlFor="amount">Amount</label>
-                <input id="amount" type="text" name="amount" onChange={changeFormDetails} />
+                <input id="amount" type="text" name="amount" onChange={changeFormDetails} value={amount} />
             </div>
 
             <button type="submit">Submit</button>
