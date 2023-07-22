c44b6ebec8e25928ebc27e47c3bb3400a8fc3087
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-22 13:35:20 +0300

    make a unified input event handler onchange

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 
 function TransactionsForm({ transactionsData, setTransactionsData }) {
-    const [category, setCategory] = useState("")
+    const [formDetails, setFormDetails] = useState({
+        date: "",
+        description: "",
+        category: "",
+        amount: 0
+    })
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        
+    }
+
+    const changeFormDetails = (event) => {
+        console.log(event.target.value)
     }
 
     return (
@@ -14,22 +23,22 @@ function TransactionsForm({ transactionsData, setTransactionsData }) {
             <h2>Add Transaction</h2>
             <div>
                 <label htmlFor="date">Date</label>
-                <input id="date" type="date"/>
+                <input id="date" name="date" type="date" onChange={changeFormDetails}/>
             </div>
 
             <div>
                 <label htmlFor="description">Description</label>
-                <textarea id="description" />
+                <textarea id="description" name="description" onChange={changeFormDetails} />
             </div>
 
             <div>
                 <label htmlFor="category">Category</label>
-                <input id="category" type="text" onChange={(event) => {setCategory(event.target.value)}} value={category} />
+                <input id="category" type="text" onChange={changeFormDetails} />
             </div>
 
             <div>
                 <label htmlFor="amount">Amount</label>
-                <input id="amount" type="text" />
+                <input id="amount" type="text" onChange={changeFormDetails} />
             </div>
 
             <button type="submit">Submit</button>
